666152996d1eaafd3a58b5dc89b5fffb7044a7aa
Samuel Adetunji <guysnacho2@gmail.com>
2021-08-20 21:13:14 -0500

    Cut out fluff

---
+++ components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav>
+    <b-navbar>
+      <template #brand>
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          <img
+            src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+            alt="Lightweight UI components for Vue.js based on Bulma"
+          />
+        </b-navbar-item>
+      </template>
+    </b-navbar>
+  </nav>
+</template>
