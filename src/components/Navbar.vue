290d8d3991b9b8a3d40f4f4cea87b9313c164543
Guysnacho <guysnacho2@gmail.com>
2021-08-30 11:52:25 -0500

    Add navbar with single global stylesheet

---
+++ src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <ClientOnly>
+    <!-- We need ClientOnly tag because vue-navigation-bar doesn't support SSR -->
+    <vue-navigation-bar :options="navbarOptions" />
+  </ClientOnly>
+</template>
+
+<script>
+import VueNavigationBar from "vue-navigation-bar"; // Documentation at https://github.com/johndatserakis/vue-navigation-bar
+
+export default {
+  data() {
+    return {
+      navbarOptions: {
+        brandImage: require("../assets/logo.png"),
+        brandImagePath: "../assets/logo.png",
+        brandImageAltText: "brand-image",
+        collapseButtonOpenColor: "#661c23",
+        collapseButtonCloseColor: "#661c23",
+        ariaLabelMainNav: "Main Navigation",
+        tooltipAnimationType: "shift-away",
+        tooltipPlacement: "bottom",
+        menuOptionsLeft: [
+          {
+            type: "link",
+            text: "Home",
+            path: "/",
+          },
+          {
+            type: "link",
+            text: "Projects",
+            path: "/projects",
+          },
+          {
+            type: "link",
+            text: "Events",
+            path: "/events",
+          },
+        ],
+      },
+    };
+  },
+  components: {
+    VueNavigationBar,
+  },
+};
+</script>
