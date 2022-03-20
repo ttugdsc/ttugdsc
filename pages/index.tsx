c6d8b9b467c9ddc280b2df5a7e54aa29e49396f8
Guysnacho <guysnacho2@gmail.com>
2022-03-20 03:47:47 -0500

    Finish desktop hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import { Card, CardContent, CardHeader } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import { Button, Typography } from "@mui/material";
 import type { NextPage } from "next";
 import Image from "next/image";
 import Head from "next/head";
@@ -17,22 +17,38 @@ const Home: NextPage = () => {
       </Head>
       <Navbar />
       <Grid container>
-        <Grid
-          container
-          direction={{ xs: "column-reverse", md: "row" }}
-          justifyContent="space-evenly"
-          alignItems="center"
-        >
-          <Grid container sx={{ width: "50%" }}>
-            <Card variant="outlined" sx={{ width: "100%" }}>
-              <CardHeader title={"Google Developer Student Club"} />
-              <CardContent>Texas Tech University</CardContent>
-            </Card>
+        <Paper elevation={2}>
+          <Grid
+            container
+            direction={{ xs: "column-reverse", md: "row" }}
+            justifyContent="space-evenly"
+            alignItems="flex-start"
+            sx={{ maxHeight: "50%" }}
+          >
+            <Grid container sx={{ width: "50%", height: "100%" }} columns={3}>
+              <Paper elevation={0} sx={{ width: "100%", p: 10 }}>
+                <Typography variant="h2" gutterBottom component="div">
+                  Google Developer Student Club
+                </Typography>
+                <Typography variant="h5" gutterBottom component="div">
+                  Texas Tech University
+                </Typography>
+                <Button
+                  variant="contained"
+                  sx={{ mt: 7, width: "80%" }}
+                  size="large"
+                  href="https://linktr.ee/ttudsc"
+                  target="_blank"
+                >
+                  Become a Member
+                </Button>
+              </Paper>
+            </Grid>
+            <Grid container sx={{ width: "50%" }}>
+              <Image src={Students} alt="Happy coding students" />
+            </Grid>
           </Grid>
-          <Grid container sx={{ width: "50%" }}>
-            <Image src={Students} alt="Happy coding students" />
-          </Grid>
-        </Grid>
+        </Paper>
       </Grid>
     </>
   );
