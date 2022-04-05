bd6375445af7e1441bd2aa861e4d7651078b9843
Guysnacho <guysnacho2@gmail.com>
2022-04-05 11:21:27 -0500

    Finalize desktop

--- components/projects/Hero.tsx
+++ components/projects/Hero.tsx
@@ -1,50 +1,56 @@
-import { Container, Grid, Stack, Typography } from "@mui/material";
+import { Grid, Stack, Typography } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import SocialButton from "../home/SocialButton";
 import { Box } from "@mui/system";
 
 const Hero = () => {
   return (
-    <Stack direction={{ xs: "column", md: "row" }}>
-      <Box sx={{ width: "720px", mx: "auto", my: 3 }}>
-        <iframe
-          width="720"
-          height="405"
-          src="https://www.youtube-nocookie.com/embed/Jqf52nThBt0?controls=0"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        ></iframe>
-      </Box>
-      <Stack direction="column" sx={{ mx: 18, justifyContent: "center" }}>
-        <Typography align="center" variant="h6" sx={{ mt: 8 }}>
-          Texas Tech Developer Student Club Mission
-        </Typography>
-        <Typography align="center" variant="body1" sx={{ mt: 6 }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate
-        </Typography>
+    <Grid container>
+      <Grid item xs={6}>
+        <Box sx={{ mx: "auto", my: 3, pl: 2 }}>
+          <iframe
+            width="720"
+            height="405"
+            src="https://www.youtube-nocookie.com/embed/Jqf52nThBt0?controls=0"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          ></iframe>
+        </Box>
+      </Grid>
+      <Grid item xs={6}>
+        <Stack direction="column" sx={{ mx: 18, mt: 4 }} spacing={4}>
+          <Typography align="center" variant="h6">
+            Texas Tech Developer Student Club Mission
+          </Typography>
+          <Typography align="center" variant="body1">
+            The purpose of the Texas Tech Google Developer Student Club is to
+            make technology more accessible and we do that by hosting workshops,
+            code-alongs, and speaker events! We also organize open source
+            projects for members to participate in.
+          </Typography>
+          <Typography align="center" variant="body1">
+            The goal is to bridge the gap between theory and application by
+            gaining real-life experience, building projects to help the
+            community, and making students ready for taking the first step into
+            their professional career.
+          </Typography>
+        </Stack>
         <Grid
           container
           sx={{
-            py: 8,
-            justifyContent: {
-              xs: "center",
-              md: "start",
-            },
+            mt: 4,
+            justifyContent: "center",
           }}
         >
           <SocialButton
             link="https://github.com/ttugdsc"
-            label="Github"
+            label="Our Projects"
             icon={<GitHubIcon />}
           />
         </Grid>
-      </Stack>
-    </Stack>
+      </Grid>
+    </Grid>
   );
 };
 
