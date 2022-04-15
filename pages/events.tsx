016f5b2b445af1f2209eb4a3f78278e5ed53686d
Samuel Adetunji <guysnacho2@gmail.com>
2022-04-15 01:11:26 -0500

    Feeling out ideas

--- pages/events.tsx
+++ pages/events.tsx
@@ -1,6 +1,9 @@
+import Navbar from "../components/Navbar";
+import { Parallax } from "react-parallax";
 import type { NextPage } from "next";
 import Head from "next/head";
-import Navbar from "../components/Navbar";
+import smiling from "../public/images/smiling.jpg";
+
 const Events: NextPage = () => {
   return (
     <>
@@ -9,6 +12,14 @@ const Events: NextPage = () => {
         <meta content="Event page of the Texas Tech Google Developer Student Club" />
       </Head>
       <Navbar />
+      <Parallax
+        blur={10}
+        bgImage="../images/smiling.jpg"
+        bgImageAlt="the cat"
+        strength={200}
+      >
+        Content goes here. Parallax height grows with content height.
+      </Parallax>
     </>
   );
 };
