ee7be77e2c5308064a88101ea4f2daa05a044110
Guysnacho <guysnacho2@gmail.com>
2022-05-12 22:03:15 -0500

    Add events through JSON

--- pages/events.tsx
+++ pages/events.tsx
@@ -1,8 +1,22 @@
 import Navbar from "../components/Navbar";
-import { Parallax } from "react-parallax";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Button, Grid, Stack, Typography } from "@mui/material";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import { Button, Grid, Paper, Stack, Typography } from "@mui/material";
+import { MyLocationOutlined } from "@mui/icons-material";
+import events from "../assets/events.json";
+
+type Event = {
+  title: string;
+  body: string;
+  date: Date;
+  venue: string;
+};
+
+const rows = [
+  // Later we'll prolly pull these from a database, for now they're hardcoded
+  ...events,
+];
 
 const Events: NextPage = () => {
   return (
@@ -12,37 +26,46 @@ const Events: NextPage = () => {
         <meta content="Event page of the Texas Tech Google Developer Student Club" />
       </Head>
       <Navbar />
-      <Parallax
-        blur={3}
-        bgImage="../images/smiling.jpg"
-        bgImageAlt="Students working"
-        strength={50}
+      <Grid
+        container
+        component={Paper}
+        elevation={1}
+        sx={{ width: "98%", mx: "auto" }}
       >
-        <Grid container>
-          <Grid item xs={5} sx={{ py: 3 }}>
-            <Stack direction="column" spacing={4} sx={{ px: 3, mx: "auto" }}>
-              <Typography variant="h5" sx={{ color: "#F44336" }}>
-                Featured
-              </Typography>
-              <Typography variant="body1">
-                My money big yeah my money tonkaContent goes here. Parallax
-                height grows with content height. You know We&apos;re pushing P
-                Content goes here. Parallax height grows with content height.
-                You know We&apos;re pushing P
-              </Typography>
-              <Button variant="contained">Learn More</Button>
-            </Stack>
-          </Grid>
-          <Grid item xs={7}>
-            <Typography variant="body1">
-              My money big yeah my money tonkaContent goes here. Parallax height
-              grows with content height. You know We&apos;re pushing P Content
-              goes here. Parallax height grows with content height. You know
-              We&apos;re pushing P
-            </Typography>
-          </Grid>
+        <Grid item xs={5} sx={{ py: 3 }}>
+          <Stack direction="column" spacing={4} sx={{ px: 3, mx: "auto" }}>
+            <Typography variant="h5">Featured</Typography>
+            <Typography variant="h4">{rows[0].title}</Typography>
+            <Typography variant="body1">{rows[0].body}</Typography>
+            <Button variant="contained">Learn More</Button>
+          </Stack>
         </Grid>
-      </Parallax>
+        <Grid item xs={3}></Grid>
+        <Grid item xs={4} sx={{ height: "75%", my: "auto" }}>
+          <Stack sx={{ pr: 5 }} direction="column" spacing={5}>
+            <Stack
+              sx={{ border: "gray" }}
+              aria-label="date-of-event"
+              spacing={5}
+              direction="row"
+            >
+              <CalendarTodayIcon sx={{ mr: 1 }} />
+              <Typography variant="body1">
+                {eval("(" + rows[0].date + ")").toDateString()}
+              </Typography>
+            </Stack>
+            <Stack
+              sx={{ border: "gray" }}
+              aria-label="location-of-event"
+              spacing={5}
+              direction="row"
+            >
+              <MyLocationOutlined sx={{ mr: 1 }} />
+              <Typography variant="body1">{rows[0].venue}</Typography>
+            </Stack>
+          </Stack>
+        </Grid>
+      </Grid>
     </>
   );
 };
