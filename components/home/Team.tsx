a93f4a33ce5a06698e6c8d48c1fbf372a3eb1cb5
Guysnacho <guysnacho2@gmail.com>
2022-05-13 12:00:57 -0500

    Fix sahil's pic again

--- components/home/Team.tsx
+++ components/home/Team.tsx
@@ -42,7 +42,7 @@ const Team = () => {
         <TeamMember
           title="Project Lead"
           name="Sahil Shamdasani"
-          link="https://media-exp1.licdn.com/dms/image/C4E03AQHIYT6y4EThfg/profile-displayphoto-shrink_800_800/0/1649251643207?e=1654732800&v=beta&t=udTvpF3QODzidzXXsSgcYSyybeeBd2mwsIfqXIGeFGs"
+          link="https://media-exp1.licdn.com/dms/image/C4E03AQHIYT6y4EThfg/profile-displayphoto-shrink_400_400/0/1649251643207?e=1657756800&v=beta&t=Y2K11zCgtkIDFFJsVYMFVEC-jCEVT2hdFpkH2KHdtqM"
           linkedIn="https://www.linkedin.com/in/sahil-shamdasani/"
         />
         <TeamMember
